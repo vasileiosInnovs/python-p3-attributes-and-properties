023c449576cbd92e145a810f7391baa2cdd10bf9
Hagee <hagee.ktr@outlook.com>
2025-05-23 23:17:54 +0300

    Generate code using attributes and properties

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,29 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='Kim', job='ITC'):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name.title()
+        
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+        
